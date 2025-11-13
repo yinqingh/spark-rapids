43129477e62c1cf9a9160f4d8c06b5e14c3fab60
Yinqing Hao <haoyinqing@gmail.com>
2025-11-13 11:50:48 +0800

    test
    Signed-off-by: Yinqing Hao <haoyinqing@gmail.com>

--- jenkins/spark-tests.sh
+++ jenkins/spark-tests.sh
@@ -307,6 +307,7 @@ com.amazonaws:aws-java-sdk-bundle:${AWS_SDK_BUNDLE_VERSION}"
     env \
       ICEBERG_TEST_REMOTE_CATALOG=1 \
       PYSP_TEST_spark_driver_memory=6G \
+      PYSP_TEST_spark_executor_memory=8g \
       PYSP_TEST_spark_jars_packages="${ICEBERG_S3TABLES_JARS}" \
       PYSP_TEST_spark_jars_repositories="${PROJECT_REPO}" \
       PYSP_TEST_spark_sql_extensions="org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions" \

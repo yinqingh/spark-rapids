8b097480c86aafd126e1fc014eb7eb116a0bf6a6
Yinqing Hao <haoyinqing@gmail.com>
2025-12-02 16:05:42 +0800

    test
    Signed-off-by: Yinqing Hao <haoyinqing@gmail.com>

--- jenkins/spark-tests.sh
+++ jenkins/spark-tests.sh
@@ -46,7 +46,7 @@ fi
 
 $WGET_CMD $PROJECT_TEST_REPO/com/nvidia/rapids-4-spark-integration-tests_$SCALA_BINARY_VER/$PROJECT_TEST_VER/rapids-4-spark-integration-tests_$SCALA_BINARY_VER-$PROJECT_TEST_VER-pytest.tar.gz
 
-RAPIDS_INT_TESTS_HOME="$ARTF_ROOT/integration_tests/"
+RAPIDS_INT_TESTS_HOME="integration_tests/"
 # The version of pytest.tar.gz that is uploaded is the one built against spark320 but its being pushed without classifier for now
 RAPIDS_INT_TESTS_TGZ="$ARTF_ROOT/rapids-4-spark-integration-tests_${SCALA_BINARY_VER}-$PROJECT_TEST_VER-pytest.tar.gz"
 
